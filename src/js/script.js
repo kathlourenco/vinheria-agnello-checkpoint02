8accf57be34c476dc274241a8a6b8d76f310dbbb
kathlourenco <kathclairesl@gmail.com>
2025-05-09 20:18:10 -0300

    codigo js base e estrutura de pastas

---
+++ src/js/script.js
@@ -0,0 +1,98 @@
+let nomevinho = "";
+let tipovinho = "";
+let dataproducao = "";
+let estoque = ""; 
+
+function cadastrarVinho() {  
+   
+    nomevinho = prompt("Insira o nome de um vinho: ");
+        
+        if (nomevinho.trim() === ""){
+            alert("Insira o nome de um vinho da nossa loja!");
+            nomevinho = prompt("Insira o nome de um vinho: ");
+        }
+        
+
+    tipovinho = prompt("Insira o tipo do vinho: Tinto, Branco ou Rosé: ");
+    tipovinho = tipovinho.trim();
+        if (tipovinho !== "tinto" && tipovinho !== "Tinto" && tipovinho !== "branco" && tipovinho !== "Branco" && tipovinho !== "rosé" && tipovinho !== "rose" && tipovinho !== "Rose" && tipovinho !== "Rosé"){
+            alert("Insira um tipo de vinho da nossa loja!");
+            tipovinho = prompt("Insira o tipo do vinho: Tinto, Branco ou Rosé: ");
+            tipovinho = tipovinho.trim();
+        }
+        if (tipovinho.toLowerCase() === "tinto") {
+            tipovinho = "Tinto";
+            alert("Sua escolha de vinho foi: " + tipovinho);
+        } else if (tipovinho.toLowerCase() === "branco") {
+            tipovinho = "Branco";
+            alert("Sua escolha de vinho foi: " + tipovinho);
+        } else if (tipovinho.toLowerCase() === "rosé" || tipovinho.toLowerCase() === "rose") {
+            tipovinho = "Rosé";
+            alert("Sua escolha de vinho foi: " + tipovinho);
+        } else {
+            alert("Insira um tipo de vinho da nossa loja!"); 
+         }
+    
+
+    dataproducao = prompt("Insira a data de produção do vinho: ").trim();
+    dataproducao = dataproducao.trim();
+       
+        if (dataproducao === "" || parseInt(dataproducao) < 1000 || parseInt(dataproducao) > 9999){
+            alert("Insira uma data de produção válida!");
+            dataproducao = parseInt(prompt("Insira a data de produção do vinho: ")); 
+        }
+        dataproducao = parseInt(String(dataproducao).trim());
+
+
+    estoque = prompt("Insira quantos vinhos deseja cadastrar: ");
+        if (estoque.trim() === "" || parseInt(estoque) <= 0) {
+            alert("Insira uma quantidade de vinhos válida no estoque!");
+            estoque = prompt("Insira quantos vinhos deseja levar: ");
+        }
+        if (estoque.trim() === "" || parseInt(estoque) <= 0) {
+            alert("Valor inválido! Por favor, insira uma quantidade maior que 0.");
+            estoque = prompt("Insira quantos vinhos deseja levar: ");
+    }
+        estoque = parseInt(estoque);
+        alert("A quantidade de vinhos é: " + estoque);
+} 
+cadastrarVinho();
+
+
+function detalhesVinho(nome, tipo, estoque, producao) { 
+    const mensagem = `Vinho cadastrado!
+    Nome do vinho:  ${nome}
+    Tipo do vinho:  ${tipo}
+    Estoque disponível:  ${estoque}
+    Ano de fabricação:  ${producao}`; 
+            
+            alert(mensagem);
+            alert("Veja os detalhes da sua compra no console!");
+                console.log("Detalhes da sua escolha: ");
+                console.log(`Nome: ${nome}`);
+                console.log(`Tipo:  ${tipo}`);
+                console.log(`Estoque:  ${estoque}`);
+                console.log(`Ano:  ${producao}`);
+    
+
+    if(estoque < 5){
+        console.log ("Classificação do estoque do vinho : "  + "  " + "Estoque baixo!");
+    } 
+    if(producao < 2015){
+            console.log("Classificação da safra do vinho : "  + "  " + "  Vinho Antigo!");
+    }
+    if(producao >= 2015 && producao <= 2019){
+            console.log("Classificação da safra do vinho : "  + "  " + "  Vinho Amadurecido!");
+    }
+    if(producao >= 2020 ){
+            console.log("Classificação da safra do vinho : "  + "  " + "  Vinho jovem!");
+    } 
+    
+} 
+ 
+detalhesVinho(nomevinho, tipovinho, estoque, dataproducao); 
+
+document.getElementById("nome").innerText = nomevinho;
+document.getElementById("tipo").innerText = tipovinho;
+document.getElementById("ano").innerText = dataproducao;
+document.getElementById("quant").innerText = estoque;    
